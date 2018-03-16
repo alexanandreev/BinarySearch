461ca52482939e793130102d7140cc98b5ce6674
Admin <Admin@WIN-118OHA59S8I>
2018-03-16 15:59:34 +0500

    2

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -8,6 +8,7 @@ namespace BinarySearch
         public static int BinarySearch(int[] array, int value)
         {
             //код поиска значения value в массиве array
+            if (array.Length == 0) return -1;
             var left = 0;
             var right = array.Length - 1;
             while (left < right)
@@ -21,11 +22,15 @@ namespace BinarySearch
                 return right;
             return -1;
         }
+
         static void Main(string[] args)
         {
             TestNegativeNumbers();
             TestNonExistentElement();
             TestSearch();
+            TestSearchRepeatNumber();
+            TestSearchInEmptyMassive();
+            TestSearchInBigMassive();
             Console.ReadKey();
         }
 
@@ -51,12 +56,43 @@ namespace BinarySearch
 
         private static void TestSearch()
         {
-            //Тестирование поиска отсутствующего элемента
+            //Тестирование поиска
             int[] Numbers = new[] { -5, -4, -3, -2, 4 };
             if (BinarySearch(Numbers, -2) != 3)
                 Console.WriteLine("Поиск работает некорректно");
             else
                 Console.WriteLine("Поиск вернул корректный результат работает корректно");
+        }
+
+        private static void TestSearchRepeatNumber()
+        {
+            //Тестирование поиска повторяющегося элемента
+            int[] Numbers = new[] { -5, -4, -2, -2, 4 };
+            if (BinarySearch(Numbers, -2) != 2)
+                Console.WriteLine("Поиск элемента, повторяющегося несколько раз работает некорректно");
+            else
+                Console.WriteLine("Поиск элемента, повторяющегося несколько раз вернул корректный результат работает корректно");
+        }
+
+        private static void TestSearchInEmptyMassive()
+        {
+            //Тестирование поиска в пустом массиве
+            int[] Numbers = new int[0];
+            if (BinarySearch(Numbers, -2) != -1)
+                Console.WriteLine("Поиск в пустом массиве работает некорректно");
+            else
+                Console.WriteLine("Поиск в пустом массиве работает корректно");
+        }
+
+        private static void TestSearchInBigMassive()
+        {
+            //Тестирование поиска в большом массиве
+            int[] Numbers = new int[100001];
+            for (int i = 0; i < Numbers.Length; i++) Numbers[i] = i;
+            if (BinarySearch(Numbers, 1000) != 1000)
+                Console.WriteLine("Поиск в массиве из 100001 элементов работает некорректно");
+            else
+                Console.WriteLine("Поиск в массиве из 100001 элементов работает корректно");
         }
     }
 }
